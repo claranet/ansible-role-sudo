e5df8e8b682aa5fcad234e239d8a318d0708996a
Delibes Bechir BKWEDOU-NGAMENI <b.bkwedou-ngameni@fr.clara.net>
2022-10-31 16:58:33 +0100

    setup molecule test

---
+++ molecule/default/tests/test_sudo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+def test_if_sudo_package_is_installed(host):
+    assert host.package("sudo").is_installed
+
+def test_sudoers_file_properties(host):
+    _sudo_file_config = host.file("/etc/sudoers")
+    assert not _sudo_file_config.contains("#includedir /etc/sudoers.d")
+    assert _sudo_file_config.mode == 0o640
+    assert _sudo_file_config.user == 'root'
+    assert _sudo_file_config.group == 'root'
+
+def test_if_sudoers_d_is_not_empty_and_contains(host):
+    assert host.file("/etc/sudoers.d").listdir() != []
